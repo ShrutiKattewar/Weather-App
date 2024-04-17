b84c12a0737a9c6e37f864bda54f45250a4bd10c
ShrutiKattewar <kattewar.shruti@gmail.com>
2024-04-17 11:33:32 +0530

    update commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Weathercard from "./components/weathercard";
 
 function App() {
+  const [searchValue, setSearchValue] = useState("pune");
+  const [tempInfo, setTempInfo] = useState({});
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=037251107f5791368a2bfc1dc33d49c9`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+
+      const { temp, humidity, pressure } = data.main;
+
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      <Weathercard {...tempInfo} />
+    </>
   );
 }
 
